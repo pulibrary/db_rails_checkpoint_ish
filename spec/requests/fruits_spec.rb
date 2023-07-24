7c3fe548e7991c17f728fc1db1555f2f1203301f
Isha <is4684@princeton.edu>
2023-07-24 17:50:31 -0400

    changed path

--- spec/requests/fruits_spec.rb
+++ spec/requests/fruits_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 RSpec.describe 'Fruits', type: :request do
   describe 'GET /index' do
     it 'returns http success' do
-      get '/fruits/index'
+      get '/fruits'
       expect(response).to have_http_status(:success)
     end
   end
