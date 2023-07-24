133eeaaf665d512031a1aacb29367cb5bb85683b
Isha <is4684@princeton.edu>
2023-07-24 17:28:44 -0400

    config changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :fruits
+  get 'fruits/:id/delete', to: 'fruits#delete', as: 'delete_fruit'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2023_07_24_160558) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "fruits", force: :cascade do |t|
+    t.string "name"
+    t.integer "calories"
+    t.text "about"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
